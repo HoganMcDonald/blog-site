c760ec464a18766c2018fbc88ecdff52cb5aa1b0
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-07-25 22:04:39 -0500

    dev env set up

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// npm install gulp gulp-autoprefixer gulp-clean-css gulp-rename gulp-sass --save-dev
+
+//required
+const gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  babel = require('gulp-babel'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  ngGraph = require('gulp-angular-architecture-graph');
+//end required
+
+//tasks
+gulp.task('scripts', () => {
+  console.log('scripts ran');
+  gulp.src('public/scripts/dev/**/*.js')
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/scripts/product'));
+});
+
+gulp.task('styles', () => {
+  console.log('styles ran');
+  gulp.src('public/styles/sass/main.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      compatibility: '*'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('angular-map', () => {
+  gulp.src('public/scripts/dev/*.js')
+    .pipe(ngGraph({
+      dest: 'architecture',
+      hideAngularServices: false
+    }));
+});
+//end tasks
+
+//watch
+gulp.task('watch:styles', () => {
+  gulp.watch('public/styles/sass/*.sass', ['styles']);
+});
+
+gulp.task('watch:scripts', () => {
+  gulp.watch('public/scripts/dev/*', ['scripts']);
+});
+
+//default task
+gulp.task('default', ['scripts', 'styles']);
